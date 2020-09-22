67c9f659e73f8dd1c8e6ae6339934496c32fcd2f
Lukas Oberholzer <lukas.oberholzer@metrohm.com>
2020-09-22 08:16:31 +0200

    stop on button released

--- app/src/main/java/com/metrohm/edu/ev3/remotecontrol/MainActivity.java
+++ app/src/main/java/com/metrohm/edu/ev3/remotecontrol/MainActivity.java
@@ -3,6 +3,7 @@ package com.metrohm.edu.ev3.remotecontrol;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,7 +15,7 @@ import lejos.robotics.RegulatedMotor;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
 	private RemoteRequestEV3 ev3;
 	private RegulatedMotor left, right;
@@ -26,18 +27,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Button left = (Button) findViewById(R.id.left);
-		Button right = (Button) findViewById(R.id.right);
-		Button stop = (Button) findViewById(R.id.stop);
-		Button forward = (Button) findViewById(R.id.forward);
-		Button backward = (Button) findViewById(R.id.backward);
-		connect = (Button) findViewById(R.id.connect);
-		left.setOnClickListener(this);
-		right.setOnClickListener(this);
-		stop.setOnClickListener(this);
-		forward.setOnClickListener(this);
-		backward.setOnClickListener(this);
+		Button left = findViewById(R.id.left);
+		Button right = findViewById(R.id.right);
+		Button forward = findViewById(R.id.forward);
+		Button backward = findViewById(R.id.backward);
+		connect = findViewById(R.id.connect);
 		connect.setOnClickListener(this);
+		left.setOnTouchListener(this);
+		right.setOnTouchListener(this);
+		forward.setOnTouchListener(this);
+		backward.setOnTouchListener(this);
 
 		if (checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
 			requestPermissions(new String[] { Manifest.permission.INTERNET }, 101);
@@ -46,21 +45,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	@Override
 	public void onClick(View v) {
-		if (v.getId() == R.id.left) new Control().execute("rotate left");
-		else if (v.getId() == R.id.right) new Control().execute("rotate right");
-		else if (v.getId() == R.id.stop) new Control().execute("stop");
-		else if (v.getId() == R.id.forward) new Control().execute("forward");
-		else if (v.getId() == R.id.backward) new Control().execute("backward");
-		else if (v.getId() == R.id.connect) {
+		if (v.getId() == R.id.connect) {
 			if (ev3 == null) {
-				new Control().execute("connect","192.168.44.245");
+				new Control().execute("connect", "192.168.44.245");
 				connect.setText("Disconnect");
-			}
-			else {
+			} else {
 				new Control().execute("disconnect");
 				connect.setText("Connect");
 			}
 		}
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				if (v.getId() == R.id.left) new Control().execute("rotate left");
+				else if (v.getId() == R.id.right) new Control().execute("rotate right");
+				else if (v.getId() == R.id.forward) new Control().execute("forward");
+				else if (v.getId() == R.id.backward) new Control().execute("backward");
+				return true;
+			case MotionEvent.ACTION_UP:
+				new Control().execute("stop");
+				return true;
+		}
+		return false;
 	}
 
 	private class Control extends AsyncTask<String, Integer, Long> {
@@ -89,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 			if (ev3 == null) return 2l;
 
-			ev3.getAudio().systemSound(1);
+//			ev3.getAudio().systemSound(1);
 
 			if (cmd[0].equals("stop")) {
 				left.stop(true);
